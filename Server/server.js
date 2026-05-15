a37a96105e3559feb780fbd5217199b41f59c19b
atlaknotssolutions <atlaknotssolutions@gmail.com>
2026-05-16 00:28:41 +0530

    task

--- Server/server.js
+++ Server/server.js
@@ -54,7 +54,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://ai-knots-website-psi.vercel.app/"],
+    origin: true,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Accept", "Authorization"],
